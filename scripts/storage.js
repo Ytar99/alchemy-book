e0128fab6f6db761a81af7526d6fc44c47bdded2
Ytar <ytar.99@yandex.ru>
2024-04-10 23:52:48 +0300

    fix: change paths to relative (1)

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,5 +1,5 @@
 import basePack from "./data/basePack.json" assert { type: "json" };
 
-import { ElementsDatabase } from "/scripts/classes/elementsDatabase.class.js";
+import { ElementsDatabase } from "./classes/elementsDatabase.class.js";
 
 export const db = new ElementsDatabase("standard", [...basePack]);
