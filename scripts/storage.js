951ed17aea9c84cc0b14a44e3e1bd11c5fd1b300
Ytar <ytar.99@yandex.ru>
2024-04-10 23:58:27 +0300

    fix

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,5 +1,5 @@
 import basePack from "./data/basePack.json" assert { type: "json" };
 
-import { ElementsDatabase } from "./classes/elementsDatabase.class.js";
+import { ElementsDatabase } from "./classes/elementsDatabase.js";
 
 export const db = new ElementsDatabase("standard", [...basePack]);
