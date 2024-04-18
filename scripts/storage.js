abd5d94f59ccc938024e54029783b42b36658e32
Ytar <ytar.99@yandex.ru>
2024-04-18 11:16:27 +0300

    update: replace JSON to object

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,4 +1,5 @@
-import basePack from "./data/basePack.json" assert { type: "json" };
+// import basePack from "./data/basePack.json" assert { type: "json" };
+import { basePack } from "./data/basePack.js";
 
 import { ElementsDatabase } from "./classes/elementsDatabase.js";
 
