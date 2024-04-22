ab2f80b3800a9e5087332131bb6c5fb9ee41ad6f
Ytar <ytar.99@yandex.ru>
2024-04-23 02:15:04 +0300

    update: add partial support for current html markup

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
-import { renderModal, renderSoundButton } from "./render.js";
+import { renderUI } from "/scripts/components/userInterfaceRender.js";
+import { renderModal } from "/scripts/components/previewRender.js";
+import { renderSoundButton } from "/scripts/components/soundRender.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderSoundButton();
   renderModal();
+  renderUI();
 });
