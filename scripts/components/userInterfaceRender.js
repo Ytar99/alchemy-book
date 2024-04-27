44c701348ac899f00da065d2245c58e4c99e64a2
Ytar <ytar.99@yandex.ru>
2024-04-27 14:04:22 +0300

    Revert "update: adjust logic"
    This reverts commit d6f80292bba1248001d2a6c8f5f256c27ae155dd.

--- scripts/components/userInterfaceRender.js
+++ scripts/components/userInterfaceRender.js
@@ -52,24 +52,18 @@ function renderElementsList() {
 
   const elements = db.getOpenedElements(db.currentCategory);
   elements.forEach((element) => {
-    const listItemWrapper = document.createElement("div");
-    listItemWrapper.classList.add("categories__elements-list__element-wrapper");
-
     const listItem = document.createElement("div");
-    // listItem.title = element.description;
+    listItem.title = element.description;
     listItem.classList.add("categories__elements-list__element");
     listItem.classList.add(element.class);
 
     const listItemText = document.createElement("span");
     listItemText.classList.add("categories__elements-list__element-text");
-    listItemText.textContent = element.text.replaceAll("&nbsp;", " ");
+    listItemText.textContent = element.text.replace("&nbsp;", " ");
 
     listItem.appendChild(listItemText);
-    listItemWrapper.appendChild(listItem);
 
-    listItem.addEventListener("click", handleAddToSelect);
-
-    list.push(listItemWrapper);
+    list.push(listItem);
   });
 
   const possibleElements = db.getPossibleElements();
@@ -82,143 +76,9 @@ function renderElementsList() {
   });
 
   listBox.replaceChildren(...list);
-
-  // function handleScroll() {
-  //   // console.log(list[0].getBoundingClientRect());
-  //   // console.log(list[3].offsetTop - 12 - 50);
-  //   // console.log(listBox.scrollTop);
-  //   const pad = 12 + 50;
-  //   const top = listBox.scrollTop;
-  //   const bottom = top + listBox.getBoundingClientRect().height;
-
-  //   const checkOpacity = (el) => {
-  //     const elHeight = el.getBoundingClientRect().height;
-  //     const offset = 0;
-
-  //     const elTop = el.offsetTop - pad;
-
-  //     let opacityTop = (elTop + elHeight - top - offset) / elHeight;
-
-  //     if (opacityTop < 0) {
-  //       opacityTop = 0;
-  //     }
-
-  //     if (opacityTop > 1) {
-  //       opacityTop = 1;
-  //     }
-
-  //     const elBottom = el.offsetTop + elHeight - pad;
-  //     let opacityBottom = (bottom - elBottom + elHeight + offset) / elHeight;
-
-  //     if (opacityBottom < 0) {
-  //       opacityBottom = 0;
-  //     }
-
-  //     if (opacityBottom > 1) {
-  //       opacityBottom = 1;
-  //     }
-
-  //     return Math.min(Number(opacityTop.toFixed(2)), Number(opacityBottom.toFixed(2)));
-  //   };
-
-  //   // console.log(bottom, list[29].offsetTop + list[29].getBoundingClientRect().height - pad);
-  //   for (let i = 0; i < list.length; i++) {
-  //     const elementChild = list[i].children[0];
-  //     const elHeight = list[i].getBoundingClientRect().height;
-
-  //     if (list[i].offsetTop - pad - elHeight / 2 > top && list[i].offsetTop + elHeight - pad + elHeight / 2 < bottom) {
-  //       elementChild.style.opacity = 1;
-  //       elementChild.style.transform = "translatey(0px)";
-  //     } else {
-  //       if (list[i].offsetTop - pad - elHeight / 2 < top) {
-  //         elementChild.style.opacity = checkOpacity(list[i]);
-  //         elementChild.style.transform = `translatey(-${(1 - checkOpacity(list[i])) * 48}px)`;
-  //       }
-
-  //       if (list[i].offsetTop + elHeight - 12 > bottom) {
-  //         elementChild.style.opacity = checkOpacity(list[i]);
-  //         elementChild.style.transform = `translatey(-${(1 - checkOpacity(list[i])) * 48}px) rotatex(-${
-  //           (1 - checkOpacity(list[i])) * 90
-  //         }deg)`;
-  //       }
-
-  //       // elementChild.style.opacity = checkOpacity(list[i]) / 3;
-  //       // elementChild.style.height = `${checkOpacity(list[i]) * 48}px`;
-  //       // elementChild.style.transform = `translatey(-${(1 - checkOpacity(list[i])) * 48}px)`;
-  //     }
-  //   }
-  // }
-
-  // handleScroll();
-
-  // listBox.addEventListener("scroll", handleScroll);
-}
-
-function getSelectsArray() {
-  return Array.from(document.querySelectorAll(".worktable__reaction-box__select"));
-}
-
-function handleAddToSelect(e) {
-  const text = e.currentTarget.textContent;
-  const name = e.currentTarget.classList[0];
-
-  const selects = getSelectsArray();
-
-  const emptySelect = selects.find((item) => item.innerHTML === "");
-
-  if (emptySelect) {
-    const resultBox = document.querySelector("#worktable__reaction-result");
-    // const list = document.querySelector("#categories__elements-list");
-
-    const clone = e.currentTarget.cloneNode(true);
-    clone.style.display = "none";
-
-    const targetRect = e.currentTarget.getBoundingClientRect();
-    const cloneForAnimation = e.currentTarget.cloneNode(true);
-    cloneForAnimation.style.position = "absolute";
-    cloneForAnimation.style.zIndex = "2";
-    cloneForAnimation.style.top = `${targetRect.top}px`;
-    cloneForAnimation.style.left = `${targetRect.left}px`;
-    cloneForAnimation.style.transition = "all 1s ease";
-    cloneForAnimation.style.pointerEvents = "none";
-    document.body.appendChild(cloneForAnimation);
-
-    const emptySelectRect = emptySelect.getBoundingClientRect();
-    const cloneForAnimationRect = cloneForAnimation.getBoundingClientRect();
-
-    cloneForAnimation.style.top = `${
-      emptySelectRect.top + Math.floor((emptySelectRect.height - cloneForAnimationRect.height) / 2)
-    }px`;
-    cloneForAnimation.style.left = `${
-      emptySelectRect.left + Math.floor((emptySelectRect.width - cloneForAnimationRect.width) / 2)
-    }px`;
-
-    setTimeout(() => {
-      cloneForAnimation.remove();
-      clone.style.display = "flex";
-      // emptySelect.appendChild(clone);
-      // clone.style.top = emptySelect.offsetTop;
-      // clone.style.left = emptySelect.offsetLeft;
-    }, 1000);
-
-    // emptySelect.innerHTML = text;
-    // emptySelect.name = name;
-    emptySelect.appendChild(clone);
-    resultBox.innerHTML = "";
-  }
 }
 
 export function renderUI() {
   renderCategoriesList();
   renderElementsList();
-
-  // const handleResize = () => {
-  //   const list = document.querySelector("#categories__elements-list");
-  //   const height = 48 + 12;
-  //   list.style.minHeight = `${Math.floor(list.getBoundingClientRect().height / height) * height}px`;
-  //   list.style.maxHeight = `${Math.floor(list.getBoundingClientRect().height / height) * height}px`;
-  //   console.log(list.getBoundingClientRect());
-  // };
-
-  // window.addEventListener("resize", handleResize);
 }
