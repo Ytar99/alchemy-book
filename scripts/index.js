3c7be6a1cdf9e07503307e9fce9378be19d8d483
Ytar <ytar.99@yandex.ru>
2024-04-30 02:19:19 +0300

    fix: themes relative path

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 import { renderUI } from "./components/userInterfaceRender.js";
 import { renderModal } from "./components/previewRender.js";
-import { renderSoundButton } from "./components/soundRender.js";
 import { renderFunctionalRow } from "./components/functionalRow.js";
-import { initTheme } from "/assets/themes/themes.js";
+import { initTheme } from "../assets/themes/themes.js";
+// import { renderSoundButton } from "./components/soundRender.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // renderSoundButton();
