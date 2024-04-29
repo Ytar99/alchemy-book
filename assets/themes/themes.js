ac0c4497c3f60a160ab75d3738869fff7dde7ba9
Ytar <ytar.99@yandex.ru>
2024-04-30 02:16:32 +0300

    update: add settings modal and themes

---
+++ assets/themes/themes.js
@@ -0,0 +1,43 @@
+export const themes = {
+  standard: {
+    name: "Классическая",
+    path: "/assets/themes/standard/",
+    backgroundImgPath: "/assets/themes/standard/bg.png",
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      background: "#ffffff",
+      text: "#8b8b8b",
+      accent1: "#FF652F",
+      accent2: "#FFE400",
+      accent3: "#14A76C",
+    },
+  },
+  dark: {
+    name: "Тёмная",
+    path: "/assets/themes/dark/",
+    backgroundImgPath: "/assets/themes/dark/bg.png",
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      background: "#272727",
+      text: "#747474",
+      accent1: "#FF652F",
+      accent2: "#FFE400",
+      accent3: "#14A76C",
+    },
+  },
+};
+
+export function initTheme(e) {
+  const savedTheme = localStorage.getItem("color-theme") || "standard";
+
+  const themeName = e?.target?.value || savedTheme;
+
+  const theme = themes[themeName];
+
+  localStorage.setItem("color-theme", themeName);
+
+  document.body.style.backgroundColor = theme.colors.background;
+  document.body.style.background = `url(${theme.backgroundImgPath}) no-repeat center center`;
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
 import { renderUI } from "./components/userInterfaceRender.js";
 import { renderModal } from "./components/previewRender.js";
 import { renderSoundButton } from "./components/soundRender.js";
+import { renderFunctionalRow } from "./components/functionalRow.js";
+import { initTheme } from "/assets/themes/themes.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderSoundButton();
+  // renderSoundButton();
+  renderFunctionalRow();
   renderModal();
   renderUI();
+  initTheme();
 });
